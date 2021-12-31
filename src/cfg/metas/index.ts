6a65dc25853f474d318425382a139ceed45c82c2
l1b3r_- <liber@ricardojrm.com>
2021-12-31 01:53:46 +0000

    feat: add metas cfg

---
+++ src/cfg/metas/index.ts
@@ -0,0 +1,8 @@
+import { PainRecord } from '@ricardo-jrm/pain';
+import { soul } from './soul';
+import { kcal } from './kcal';
+
+export const metas: PainRecord = {
+  soul,
+  kcal,
+};
---
+++ src/cfg/metas/soul.ts
@@ -0,0 +1,8 @@
+import { PainData } from '@ricardo-jrm/pain';
+
+export const soul: PainData = {
+  name: 'SOUL',
+  description: 'SOUL Explorer',
+  domain: 'https://phantasma.io/',
+  logo: '/static/v1/img/soul-logo.png',
+};
