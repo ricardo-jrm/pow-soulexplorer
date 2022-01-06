901e974a6cbc929ff320f61932102d4c1184262f
l1b3r_- <liber@ricardojrm.com>
2022-01-06 12:11:58 +0000

    feat: chains list

--- src/containers/ViewNexus/index.tsx
+++ src/containers/ViewNexus/index.tsx
@@ -1,10 +1,98 @@
 import React, { useMemo } from 'react';
+import { useFury } from '@ricardo-jrm/fury';
 import { Box } from '@ricardo-jrm/fury/dist/mui';
 import { useEcho } from '@ricardo-jrm/echo';
 import { Text } from '../../components/Text';
 import { NavTabs, NavTabRecord } from '../../components/NavTabs';
+import { ItemsList, ListRow, ListCol } from '../../components/ItemsList';
+import { chains } from '../../mocks/chains';
 
-const ChainsComponent = () => <>CHAINS</>;
+const chain = chains.main;
+
+interface NameCellProps {
+  name: string;
+}
+const NameCell = ({ name }: NameCellProps) => {
+  const { furyActive } = useFury();
+
+  return (
+    <Text
+      link={{
+        href: `/chain?id=${name}`,
+      }}
+      sx={{
+        color: furyActive.palette.secondary.main,
+      }}
+    >
+      {name}
+    </Text>
+  );
+};
+
+interface AddressCellProps {
+  address: string;
+}
+const AddressCell = ({ address }: AddressCellProps) => {
+  const { furyActive } = useFury();
+
+  return (
+    <Text
+      link={{
+        href: `/account?address=${address}`,
+      }}
+      sx={{
+        color: furyActive.palette.secondary.main,
+      }}
+      clipboard
+      spacing={1}
+    >
+      {address}
+    </Text>
+  );
+};
+
+interface HeightCellProps {
+  height: string;
+}
+const HeightCell = ({ height }: HeightCellProps) => {
+  return <Text>{`#${height}`}</Text>;
+};
+
+const chainsCols: ListCol[] = [
+  {
+    label: 'label-name',
+    cols: 3,
+  },
+  {
+    label: 'label-address',
+    cols: 6,
+  },
+  {
+    label: 'label-height',
+    cols: 3,
+  },
+];
+const chainsRows: ListRow[] = [
+  [
+    {
+      label: 'label-name',
+      cols: 3,
+      component: <NameCell name={chain.name} />,
+    },
+    {
+      label: 'label-address',
+      cols: 6,
+      component: <AddressCell address={chain.address} />,
+    },
+    {
+      label: 'label-blockheight',
+      cols: 3,
+      component: <HeightCell height={`${chain.blockHeight}`} />,
+    },
+  ],
+];
+
+const ChainsComponent = () => <ItemsList cols={chainsCols} rows={chainsRows} />;
 
 const TokensComponent = () => <>TOKENS</>;
 
--- src/containers/ViewTransaction/index.tsx
+++ src/containers/ViewTransaction/index.tsx
@@ -1,5 +1,6 @@
 import React, { useMemo } from 'react';
 import { useRouter } from 'next/router';
+import { useFury } from '@ricardo-jrm/fury';
 import { Box } from '@ricardo-jrm/fury/dist/mui';
 import { useEcho } from '@ricardo-jrm/echo';
 import { Text } from '../../components/Text';
@@ -7,6 +8,13 @@ import { NavTabs, NavTabRecord } from '../../components/NavTabs';
 import { NotFound } from '../../components/404';
 import { transactions, Tx } from '../../mocks/tx';
 import { TxOverview } from '../../components/TxOverview';
+import { ItemsList, ListRow, ListCol } from '../../components/ItemsList';
+
+// eslint-disable-next-line operator-linebreak
+const { events } =
+  transactions[
+    '6DC8D95C32D1517DE55EB4D8A46BF23235CE3DCFA38079398AC01F5C182CFF55'
+  ];
 
 interface ScriptProps {
   script?: string;
@@ -35,7 +43,135 @@ const ScriptComponent = ({ script }: ScriptProps) => {
   );
 };
 
-const EventsComponent = () => <>Events</>;
+interface KindCellProps {
+  kind: string;
+}
+const KindCell = ({ kind }: KindCellProps) => <Text>{kind}</Text>;
+
+interface AddressCellProps {
+  address: string;
+}
+const AddressCell = ({ address }: AddressCellProps) => {
+  const { furyActive } = useFury();
+
+  return (
+    <Text
+      link={{
+        href: `/account?address=${address}`,
+      }}
+      sx={{
+        color: furyActive.palette.secondary.main,
+      }}
+    >
+      {address}
+    </Text>
+  );
+};
+
+interface ContractCellProps {
+  contract: string;
+}
+const ContractCell = ({ contract }: ContractCellProps) => (
+  <Text>{contract}</Text>
+);
+
+interface ContentCellProps {
+  content: string;
+}
+const ContentCell = ({ content }: ContentCellProps) => (
+  <Text clipboard spacing={1}>
+    {content}
+  </Text>
+);
+
+const eventCols: ListCol[] = [
+  {
+    label: 'label-kind',
+    cols: 2,
+  },
+  {
+    label: 'label-address',
+    cols: 2,
+  },
+  {
+    label: 'label-contract',
+    cols: 2,
+  },
+  {
+    label: 'label-content',
+    cols: 6,
+  },
+];
+const eventRows: ListRow[] = [
+  [
+    {
+      label: 'label-kind',
+      cols: 2,
+      component: <KindCell kind={events[0].kind} />,
+    },
+    {
+      label: 'label-address',
+      cols: 2,
+      component: <AddressCell address={events[0].address} />,
+    },
+    {
+      label: 'label-contract',
+      cols: 2,
+      component: <ContractCell contract={events[0].contract} />,
+    },
+    {
+      label: 'label-content',
+      cols: 6,
+      component: <ContentCell content={events[0].content} />,
+    },
+  ],
+  [
+    {
+      label: 'label-kind',
+      cols: 2,
+      component: <KindCell kind={events[1].kind} />,
+    },
+    {
+      label: 'label-address',
+      cols: 2,
+      component: <AddressCell address={events[1].address} />,
+    },
+    {
+      label: 'label-contract',
+      cols: 2,
+      component: <ContractCell contract={events[1].contract} />,
+    },
+    {
+      label: 'label-content',
+      cols: 6,
+      component: <ContentCell content={events[1].content} />,
+    },
+  ],
+  [
+    {
+      label: 'label-kind',
+      cols: 2,
+      component: <KindCell kind={events[2].kind} />,
+    },
+    {
+      label: 'label-address',
+      cols: 2,
+      component: <AddressCell address={events[2].address} />,
+    },
+    {
+      label: 'label-contract',
+      cols: 2,
+      component: <ContractCell contract={events[2].contract} />,
+    },
+    {
+      label: 'label-content',
+      cols: 6,
+      component: <ContentCell content={events[2].content} />,
+    },
+  ],
+];
+
+const EventsComponent = () => <ItemsList cols={eventCols} rows={eventRows} />;
 
 /**
  * ViewTransaction
